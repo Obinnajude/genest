405a259c251255320f59ad8f87fc0a549dbd7407
Ugwuododo Jude Obinna <jobinna50@gmail.com>
2022-02-28 01:13:28 -0700

    this is my first commit of the code

---
+++ src/cal.py
@@ -0,0 +1,42 @@
+# Please make sure your code is running fine before pushing it online to github and add your code only where specified 
+# Add only one feature 
+
+def AddTwoNumbers(numb,numb1):
+    print("enter your name here...")
+    #add your code here to add two numbers and print the answer
+
+def multiplyTwoNumbers(numb,numb1):
+  print("enter your name here...")
+   #add your code here to multiply two numbers and print the answer
+
+def AddThreeNumbers(numb, numb1):
+  num3 = int(input("Enter third number: "))
+  print("enter your name here...")
+   # add your code here to add three numbers and print the answer
+
+def multiplyThreeNumbers(numb, numb1):
+ num3 = int(input("Enter third number: "))
+ print("enter your name here...")
+ # add your code here to multiply three numbers here and print the answer
+
+def main():
+  try:
+    ask = input("What do you want to do? (hint Emter add2, muti2, add3, muti3): ")
+    num = int(input("Enter first number: "))
+    num1 = int(input("Enter second number: "))
+
+    if ask == "add2":
+      AddTwoNumbers(num,num1)
+    elif ask == "muti2":
+      multiplyTwoNumbers(num,num1)
+    elif ask == "add3":
+      AddThreeNumbers(num, num1)
+    elif ask == "muti3":
+      multiplyThreeNumbers(num,num1)
+    else:
+      print("Bye Enter the correct information")
+  except:
+    print("Enter a whole number or the right value. Bye!")  
+if __name__ == '__main__':
+    main()
+    
---
+++ src/quiz.py
@@ -0,0 +1,42 @@
+questions = {
+  "How many states does United State has?":"50",
+  "How many days are there in a year?": "365"
+
+  # ***add more questions here: do it as the one above and use comma to separate two question***
+  #**comment your name above the question**
+
+}
+
+
+#Please, don't change anything below this line
+
+def main():
+ 
+  print("Hello learners!")
+  # ans = trivia_fetch(6)
+  # print(ans)
+
+
+  # The first answer for the quiz is 0 in order to proceed 
+  def quiz(dic):
+    score = 0
+    qnumb = 0
+    for q, a in questions.items():
+        print(q)
+        qnumb = qnumb + 1
+        ans = input("Enter your answer: ")
+        if ans == a:
+          print("\n Hurray!! you are right\n")
+          score = score + 1
+         
+        else:
+          print("\n You failed it!!! Try again\n")
+          break
+    print(f"Your Score is {score/qnumb*100}%")
+
+# Here we passed the question to the quiz function
+  quiz(questions)
+  
+
+if __name__=="__main__":
+  main()
